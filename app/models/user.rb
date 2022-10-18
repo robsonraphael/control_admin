adeb661e48b54aeb46486defad0f2274360db41d
Robson Raphael <robsonraphael2001@gmail.com>
2022-10-18 01:42:59 -0300

    First commit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  enum role: %i[user moderator admin]
+  after_initialize :set_defaut_role, if: :new_record?
+  def set_default_role
+    self.role ||= :user
+  end
+end
